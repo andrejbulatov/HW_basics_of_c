44bc186386399ae6393ea7877ec823e4f8652413
Andrei Bulatov <andbulatov@mail.ru>
2023-02-11 14:13:36 +0300

    Add output all even numbers up to the specified

---
+++ HW_seminar1_task8/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+System.Console.Write("Введите число N: ");
+string numberN = Console.ReadLine();
+int number = Convert.ToInt32(numberN);
+
+int count = 2;
+
+if (number <= 1)
+{
+  System.Console.WriteLine($"Вы ввели некорректное значение числа N = {number}. Для вывода всех четных чисел от 1 до N, N должно быть больше 1");
+}
+
+System.Console.Write($"Все четные числа от 1 до {number}: ");
+while (count <= number)
+{
+  System.Console.Write($"{count} ");
+  count = count + 2;
+}
