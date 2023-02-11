d6e8271939ecda9cbf7ce0bc09375df130c4d869
Andrei Bulatov <andbulatov@mail.ru>
2023-02-11 13:45:14 +0300

    Add comparison of three numbers and definition max

---
+++ HW_seminar1_task4/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+System.Console.Write("Введите первое число для сравнения: ");
+string numberA = Console.ReadLine();
+int numberFirst = Convert.ToInt32(numberA);
+
+System.Console.Write("Введите второе число для сравнения: ");
+string numberB = Console.ReadLine();
+int numberSecond = Convert.ToInt32(numberB);
+
+System.Console.Write("Введите третье число для сравнения: ");
+string numberC = Console.ReadLine();
+int numberThird = Convert.ToInt32(numberC);
+
+int maxNumber = numberFirst;
+
+if (maxNumber < numberSecond)
+{
+  maxNumber = numberSecond;
+}
+if (maxNumber < numberThird)
+  {
+    maxNumber = numberThird;
+  }
+
+System.Console.WriteLine($"Максимальным числом из {numberFirst}, {numberSecond} и {numberThird} является {maxNumber}");
