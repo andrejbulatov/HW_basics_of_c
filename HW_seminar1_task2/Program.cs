b024daf6fe565693c8bce52049cace474c19bf6b
Andrei Bulatov <andbulatov@mail.ru>
2023-02-11 13:21:09 +0300

    Add comparison of two numbers and definition max and min

---
+++ HW_seminar1_task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+System.Console.Write("Введите первое число для сравнения: ");
+string numberA = Console.ReadLine();
+int numberFirst = Convert.ToInt32(numberA);
+
+System.Console.Write("Введите второе число для сравнения: ");
+string numberB = Console.ReadLine();
+int numberSecond = Convert.ToInt32(numberB);
+
+if (numberFirst == numberSecond)
+{
+  System.Console.WriteLine($"Вы ввели одинаковые цифры {numberFirst} два раза, введите разные цифры для сравнения");
+}
+if (numberFirst > numberSecond)
+{
+  System.Console.WriteLine($"Максимальное значение max={numberFirst}, минимальное значение min={numberSecond}");
+}
+if (numberFirst < numberSecond)
+{
+  System.Console.WriteLine($"Максимальное значение max={numberSecond}, минимальное значение min={numberFirst}");
+}
+
+
+
