b325f667fe59c6550ef56a5ed76c08880309805a
Andrei Bulatov <andbulatov@mail.ru>
2023-02-11 13:59:14 +0300

    Add definition even and odd numbers

---
+++ HW_seminar1_task6/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+System.Console.Write("Введите число для определения его четности: ");
+string numberA = Console.ReadLine();
+int number = Convert.ToInt32(numberA);
+
+int remainderDivision = number % 2;
+
+if (remainderDivision == 0)
+{
+  System.Console.WriteLine($"Число {number} является четным");
+}
+else
+{
+  System.Console.WriteLine($"Число {number} является нечетным");
+}
