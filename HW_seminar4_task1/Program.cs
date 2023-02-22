41601c32bd3c0716b9fa5edcb7aa84fb95a5262f
Andrei Bulatov <andbulatov@mail.ru>
2023-02-22 08:46:05 +0300

    Add get number in a power

---
+++ HW_seminar4_task1/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 25:
+// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int InputInt(string message)
+{
+  System.Console.Write($"{message} ");
+  int value;
+  bool isCorrect = int.TryParse(Console.ReadLine(), out value);
+  if (isCorrect)
+  {
+    return value;
+  }
+  System.Console.WriteLine("Вы ввели не число или не целое число");
+  Environment.Exit(1);
+  return 0;
+}
+
+bool ValidateNumber(int naturalNumber)
+{
+  if (naturalNumber >= 1)
+  {
+    return true;
+  }
+  System.Console.WriteLine("Введенное вами число не является натуральным.");
+  return false;
+}
+
+int GetNumberInPower(int number, int power)
+{
+  int result = number;
+  for (int i = 1; i < power; i++)
+  {
+    result = result * number;
+  }
+  return result;
+}
+
+int number = InputInt("Введите целое число A, которое нужно возвести в степень: ");
+int power = InputInt("Введите натуральное число B - степень, в которую нужно возвести число A: ");
+if(ValidateNumber(power))
+{
+  System.Console.WriteLine($"При возведении числа {number} в степень {power} получается число {GetNumberInPower(number, power)}"); 
+}
