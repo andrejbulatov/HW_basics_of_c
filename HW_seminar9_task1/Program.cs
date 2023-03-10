f067d31338a89fcbad6391f7b81b171763a552d3
Andrei Bulatov <andbulatov@mail.ru>
2023-03-10 23:05:27 +0300

    Add output even numbers between two numbers using recursion

---
+++ HW_seminar9_task1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу,
+// которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+int InputNumber(string prompt)
+{
+  System.Console.Write($"{prompt}");
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+
+void ShowEvenNumbers(int numFirst, int numSecond)
+{
+  if (numFirst > numSecond)
+  {
+    return;
+  }
+  
+  if (numFirst % 2 == 0) System.Console.WriteLine($"{numFirst}");
+  ShowEvenNumbers(numFirst + 1, numSecond);
+  
+}
+
+
+int numberFirst = InputNumber("Введите первое число ");
+int numberSecond = InputNumber("Введите второе число ");
+ShowEvenNumbers(numberFirst, numberSecond);
