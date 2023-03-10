da07f66092c77bd0debba910744688d6b3bc3d34
Andrei Bulatov <andbulatov@mail.ru>
2023-03-10 23:50:55 +0300

    Add count value of Akkermans function

---
+++ HW_seminar9_task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 3: Напишите программу 
+// вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n. 
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+int InputNumber(string prompt)
+{
+  System.Console.Write($"{prompt}");
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int CountFunctionAkkerman(int numFirst, int numSecond)
+{
+  if (numFirst == 0) return numSecond + 1;
+  if (numFirst > 0 && numSecond == 0) return CountFunctionAkkerman(numFirst - 1, 1);
+  return CountFunctionAkkerman(numFirst - 1, CountFunctionAkkerman(numFirst, numSecond - 1));
+}
+
+int numberFirst = InputNumber("Введите первое число больше или равное нулю ");
+int numberSecond = InputNumber("Введите второе число больше или равное нулю ");
+System.Console.WriteLine($"Функция Аккермана для двух чисел A({numberFirst},{numberSecond}) = {CountFunctionAkkerman(numberFirst, numberSecond)}");
